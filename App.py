dca3b7b46d5e1ac161c11c45450dae8c4c3e8521
Hassanibrar632 <hassanibrar632@gmail.com>
2024-12-03 18:01:05 +0500

    requirments.txt

---
+++ App.py
@@ -0,0 +1,12 @@
+"""
+This will be the main APP
+features to be added:
+
+1. Youtube Video downloader.
+2. Transcribing Videos.
+3. Adding Vector Database.
+4. Creating Embeddings.
+5. Choosing RAG model for Q&A.
+6. Add UI for the user to communicate.
+
+"""
